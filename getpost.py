3110a90cfb533b3c7d1a879b3068bd4f96571760
nbelisle <nicolebelisle121@gmail.com>
2020-03-25 14:49:50 -0400

    Practiced get requests

---
+++ getpost.py
@@ -0,0 +1,25 @@
+import requests
+
+# r = requests.get("https://api.github.com/repos/dward2/BME547/branches")
+# print(type(r))
+# print(r)
+# print(r.text)
+# print("")
+# answer = r.json()
+# print(type(answer))
+# print(answer)
+# for branch in answer:
+#     print("Name of branch is {}".format(branch["name"]))
+
+# r = requests.get("https://api.github.com/BME547_Class/nicolebelisle/blood_analysis/branches")
+# print(type(r))
+# print(r)
+# print(r.text)
+# print("")
+# answer = r.json()
+# print(type(answer))
+# print(answer)
+# for branch in answer:
+#     print("Name of branch is {}".format(branch["name"]))
+
+r = requests.post
