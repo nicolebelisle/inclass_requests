31997ab3588ee3e58eec15d3a4d54d92d2d4ceff
nbelisle <nicolebelisle121@gmail.com>
2020-03-25 15:01:53 -0400

    practiced with regions

--- getpost.py
+++ getpost.py
@@ -22,12 +22,15 @@ import requests
 # for branch in answer:
 #     print("Name of branch is {}".format(branch["name"]))
 
-request_json = {"one": "Spain", "two": "Canada"}
-r = requests.post("http://vcm.7631.vm.duke.edu:5000/compare", json=request_json)
-print(r)
-print(r.status_code)
-print(r.text)
-if r.status_code == 200:
-    print(r.json)
-else:
-    print("There was an error with status code of {}".format(r.status_code)
+r = requests.get("https://vcm-7631.vm.duke.edu:5000/regions")
+print(r.json)
+
+# request_json = {"one": "Spain", "two": "Canada"}
+# r = requests.post("http://vcm-7631.vm.duke.edu:5000/compare", json=request_json)
+# print(r)
+# print(r.status_code)
+# print(r.text)
+# if r.status_code == 200:
+#     print(r.json)
+# else:
+#     print("There was an error with status code of {}".format(r.status_code)
